b8062b7b9547807cdbb6d02614a839acd9a44d4f
ToryanBurak <103573489+ToryanBurak@users.noreply.github.com>
2024-10-15 23:43:48 +0300

    a

--- BurSimEmFur-Ecommerce/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BurSimEmFur_Ecommerce.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
