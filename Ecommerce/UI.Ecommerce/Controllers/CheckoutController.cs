279d3ab7372c44093717824de56f5ac02ad9b133
emreaktasq <118172596+emreaktasq@users.noreply.github.com>
2025-01-17 01:18:50 +0300

    Checkout sayfasi olusturuldu

---
+++ Ecommerce/UI.Ecommerce/Controllers/CheckoutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Ecommerce.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmMail()
+        {
+
+            return View();
+        }
+    }
+}
