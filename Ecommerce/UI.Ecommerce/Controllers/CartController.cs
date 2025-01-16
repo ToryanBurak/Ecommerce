13d32c5f8a88a2375914a528bb2705671fa5826e
emreaktasq <118172596+emreaktasq@users.noreply.github.com>
2025-01-16 18:49:50 +0300

    Card eklendi, Checkout tamamlandı.

---
+++ Ecommerce/UI.Ecommerce/Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Ecommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: /Cart/Index
+        public IActionResult Index()
+        {
+         
+            return View(); 
+        }
+    }
+}
---
+++ Ecommerce/UI.Ecommerce/Controllers/CheckoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Ecommerce.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        [HttpGet]
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DetailsPost()
+        {
+            return RedirectToAction("Complete");
+        }
+
+        public IActionResult Complete()
+        {
+            return View();
+        }
+    }
+}
