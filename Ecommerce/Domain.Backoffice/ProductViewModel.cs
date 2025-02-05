4009d308d024772748748c5bca320997040fdae2
Burak Toryan <burak.toryan@ngi.com.tr>
2025-02-06 02:09:24 +0300

    Son düzenlemeler yapıldı

--- Ecommerce/Domain.Backoffice/ProductViewModel.cs
+++ Ecommerce/Domain.Backoffice/ProductViewModel.cs
@@ -11,7 +11,7 @@ namespace Domain.Backoffice
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public string Amount { get; set; } = null!;
+        public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public int ImageUrlId { get; set; }
         public string ImageUrl { get; set; }
